6b50606366851ad1d38f24dfd1703f287bab60ee
Christian Eichert <c@zp1.net>
2025-05-26 04:00:07 +0200

    modified:   gumicorn/app/wsgiapp.py

--- gumicorn/app/wsgiapp.py
+++ gumicorn/app/wsgiapp.py
@@ -13,32 +13,33 @@ class WSGIApplication(Application):
     def init(self, parser, opts, args):
         self.app_uri = None
 
-        if opts.paste:
-            from .pasterapp import has_logging_config
+        if self.cfg is not None:
+            if opts.paste:
+                from .pasterapp import has_logging_config
 
-            config_uri = os.path.abspath(opts.paste)
-            config_file = config_uri.split('#')[0]
+                config_uri = os.path.abspath(opts.paste)
+                config_file = config_uri.split('#')[0]
 
-            if not os.path.exists(config_file):
-                raise ConfigError("%r not found" % config_file)
+                if not os.path.exists(config_file):
+                    raise ConfigError("%r not found" % config_file)
 
-            self.cfg.set("default_proc_name", config_file)
-            self.app_uri = config_uri
+                self.cfg.set("default_proc_name", config_file)
+                self.app_uri = config_uri
 
-            if has_logging_config(config_file):
-                self.cfg.set("logconfig", config_file)
+                if has_logging_config(config_file):
+                    self.cfg.set("logconfig", config_file)
 
-            return
+                return
 
-        if len(args) > 0:
-            self.cfg.set("default_proc_name", args[0])
-            self.app_uri = args[0]
+            if len(args) > 0:
+                self.cfg.set("default_proc_name", args[0])
+                self.app_uri = args[0]
 
     def load_config(self):
         super().load_config()
 
         if self.app_uri is None:
-            if self.cfg.wsgi_app is not None:
+            if self.cfg and self.cfg.wsgi_app is not None:
                 self.app_uri = self.cfg.wsgi_app
             else:
                 raise ConfigError("No application module specified.")
@@ -48,10 +49,11 @@ class WSGIApplication(Application):
 
     def load_pasteapp(self):
         from .pasterapp import get_wsgi_app
-        return get_wsgi_app(self.app_uri, defaults=self.cfg.paste_global_conf)
+        if self.cfg is not None:
+            return get_wsgi_app(self.app_uri, defaults=self.cfg.paste_global_conf)
 
     def load(self):
-        if self.cfg.paste is not None:
+        if self.cfg and self.cfg.paste is not None:
             return self.load_pasteapp()
         else:
             return self.load_wsgiapp()
