03c1b14455f1a96fd9475a01b137fdec9078d897
Christian Eichert <c@zp1.net>
2025-06-02 08:04:29 +0200

    modified:   gumicorn/__init__.py

--- gumicorn/__init__.py
+++ gumicorn/__init__.py
@@ -2,6 +2,20 @@
 # This file is part of gumicorn released under the MIT license.
 # See the NOTICE for more information.
 
+from dotenv import load_dotenv
+import os
+import sentry_sdk
+
+# Load environment variables from .env file (only needed locally)
+load_dotenv()
+
+# Initialize Sentry
+sentry_sdk.init(
+    dsn=str(os.getenv("GLITCHTIP_DSN")),
+    traces_sample_rate=1.0,
+    environment="production" if os.getenv("CI") else "development"
+)
+
 version_info = (23, 0, 0)
 __version__ = ".".join([str(v) for v in version_info])
 SERVER = "gumicorn"
